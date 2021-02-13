62daae17f4d80b639cffd9a15e01498782b6062b
Conner Martin <connerjm2@gmail.com>
2021-02-12 17:54:51 -0800

    Added some comments and linked jquery and bootstrap.

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function ()//Encouraged when using jQuery.
 {
+    /* DOM Elements */
+
+    /* Variables */
+
+    /* Primary Functions. */
+
+    /* Helper Functions. */
+
+    /* Attaching Listeners. */
+
+    /* Function Calls. */
 
 });
